1e6cf67d8c66706255648c13eb9ec24b5f5fac4b
Yinandi <yinandi.tech@gmail.com>
2022-08-20 18:55:19 -0400

    first commit

---
+++ js/efectos.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+ 
+ //Scroll Elementos Meu
+ 	var acercaDe = $('#acerca-de').offset().top,
+ 		menu = $('#platillos').offset().top,
+ 		galeria = $('#galeria').offset().top,
+ 		ubicacion = $('#ubicacion').offset().top;
+
+//Boton Acerca de
+ 	$('#btn-acerca-de').on('click',function(e){
+ 		e.preventDefault();
+ 		$('html, body').animate({
+ 			scrollTop: 380
+ 		}, 500);
+ 	});
+
+ 	$('#btn-menu').on('click',function(e){
+ 		e.preventDefault();
+ 		$('html, body').animate({
+ 			scrollTop: menu-40
+ 		});
+ 	});
+
+ 	$('#btn-galeria').on('click',function(e){
+ 		e.preventDefault();
+ 		$('html, body').animate({
+ 			scrollTop: galeria-40
+ 		});
+ 	});
+
+ 	$('#btn-ubicacion').on('click',function(e){
+ 		e.preventDefault();
+ 		$('html, body').animate({
+ 			scrollTop: ubicacion-40
+ 		});
+ 	});
+ //Efecto menu
+	$('.menu a').each(function(index, elemento){
+		$(this).css({
+			'top': '-200px'
+		});
+
+		$(this).animate({
+			'top': '0'
+		}, 2000 + (index * 500));
+	});
+
+ //Efecto Header
+ 	if($(window).width()>800){
+ 		$('header .textos').css({
+ 			opacity: 0,
+ 			marginTop:0
+ 		});
+
+ 		$('header .textos').animate({
+ 			opacity: 1,
+ 			marginTop:'-52px'
+ 		}, 1500);
+ 	}
+ 
+ 
+});
